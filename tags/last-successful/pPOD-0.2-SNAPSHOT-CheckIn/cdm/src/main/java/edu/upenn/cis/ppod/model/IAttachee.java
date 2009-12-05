bd2dbabfc4c0b696604a1d45b4465532ab4bcd48
snd297 <snd297@ca718cfa-cfbe-11de-a27d-972f07a5c692>
2009-12-05 07:58:00 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-pPOD-0.2-SNAPSHOT-CheckIn-403.

---
+++ tags/last-successful/pPOD-0.2-SNAPSHOT-CheckIn/cdm/src/main/java/edu/upenn/cis/ppod/model/IAttachee.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2009 Trustees of the University of Pennsylvania
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS of ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.upenn.cis.ppod.model;
+
+import java.util.Set;
+
+/**
+ * An object to which we can add and remove attachments.
+ * 
+ * @author Sam Donnelly
+ */
+public interface IAttachee {
+
+	/**
+	 * Add an attachment to this {@code IAttachee}.
+	 * 
+	 * @param attachment to be attached
+	 * @return this {@code IAttachee}
+	 */
+	IAttachee addAttachment(Attachment attachment);
+
+	/**
+	 * Get the attachments of this {@code IAttachee}.
+	 * 
+	 * @return the attachments of this {@code IAttachee}
+	 */
+	Set<Attachment> getAttachments();
+
+	/**
+	 * Get all attachments within the given namespace.
+	 * 
+	 * @param namespace the namespace
+	 * @return all attachments within the given namespace
+	 */
+	Set<Attachment> getAttachmentsByNamespace(String namespace);
+
+	/**
+	 * Get all attachments within the given namespace and of the given type.
+	 * 
+	 * @param namespace the namespace
+	 * @param type the attachment type
+	 * @return all attachments within the given namespace and of the given type.
+	 */
+	Set<Attachment> getAttachmentsByNamespaceAndType(String namespace,
+			String type);
+
+	/**
+	 * Remove {@code attachment} from this {@code IAttachee}'s attachments.
+	 * 
+	 * @param attachment to be removed
+	 * @return {@code true} if the set contained the specified element
+	 */
+	boolean removeAttachment(Attachment attachment);
+
+}
