1c79989b24d748d4edd21888efafd07c9bd908a3
snd297 <snd297@ca718cfa-cfbe-11de-a27d-972f07a5c692>
2009-12-06 03:45:29 +0000

    clover report

---
+++ site/clover/edu/upenn/cis/ppod/model/PersistentObject.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":76,"id":1046,"methods":[{"el":48,"sc":2,"sl":48},{"el":52,"sc":2,"sl":50},{"el":56,"sc":2,"sl":54},{"el":75,"sc":2,"sl":64}],"name":"PersistentObject","sl":33}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {"test_3":{"methods":[{"sl":48}],"name":"save","pass":true,"statements":[]}}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [3], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
