f6d2a4c4d9aa64d10ba18d1f9c0a139ef578ebf8
snd297 <snd297@ca718cfa-cfbe-11de-a27d-972f07a5c692>
2009-12-06 03:37:05 +0000

    clover report

---
+++ site/clover/edu/upenn/cis/dbgroup/common/messages/IMessage.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":37,"id":1981,"methods":[],"name":"IMessage","sl":22},{"el":25,"id":1981,"methods":[],"name":"IMessage.Level","sl":23},{"el":35,"id":1981,"methods":[],"name":"IMessage.IFactory","sl":33}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
---
+++ site/clover/edu/upenn/cis/ppod/saveorupdate/hibernate/SaveOrUpdateOTUSetHibernate.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":85,"id":2910,"methods":[{"el":55,"sc":2,"sl":48},{"el":84,"sc":2,"sl":57}],"name":"SaveOrUpdateOTUSetHibernate","sl":43}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
