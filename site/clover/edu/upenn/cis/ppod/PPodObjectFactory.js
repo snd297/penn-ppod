ea6d78a5dbb9007db30b0f8f37307e8387230e64
snd297 <snd297@ca718cfa-cfbe-11de-a27d-972f07a5c692>
2009-12-06 04:05:18 +0000

    clover report

---
+++ site/clover/edu/upenn/cis/ppod/PPodObjectFactory.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":54,"id":3034,"methods":[{"el":39,"sc":2,"sl":36},{"el":53,"sc":2,"sl":41}],"name":"PPodObjectFactory","sl":34}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
---
+++ site/clover/edu/upenn/cis/ppod/util/SetDocIdVisitor.js
@@ -0,0 +1,10 @@
+var clover = new Object();
+
+// JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
+clover.pageData = {"classes":[{"el":59,"id":1784,"methods":[{"el":33,"sc":2,"sl":31},{"el":38,"sc":2,"sl":35},{"el":43,"sc":2,"sl":40},{"el":48,"sc":2,"sl":45},{"el":52,"sc":2,"sl":50},{"el":57,"sc":2,"sl":54}],"name":"SetDocIdVisitor","sl":29}]}
+
+// JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
+clover.testTargets = {}
+
+// JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
