e07a98ce483d10fac43cabf0e76141c0539a1581
snd297 <snd297@ca718cfa-cfbe-11de-a27d-972f07a5c692>
2010-03-03 04:05:27 +0000

    Working on end-to-end functional tests.

--- trunk/cdm/src/test/java/edu/upenn/cis/ppod/util/JettyWebServer.java
+++ trunk/cdm/src/test/java/edu/upenn/cis/ppod/util/JettyWebServer.java
@@ -4,11 +4,15 @@ import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Sam Donnelly
  */
-public class JettyService implements IService {
+public class JettyWebServer implements IService {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	private Server server;
 
@@ -30,11 +34,14 @@ public class JettyService implements IService {
 		// parse it
 		server.setHandler(wac);
 		server.setStopAtShutdown(true);
+		logger.info("starting up jetty...");
 		server.start();
-
+		logger.info("...started");
 	}
 
 	public void stop() throws Exception {
+		logger.info("stopping jetty...");
 		server.stop();
+		logger.info("...stopped");
 	}
 }
